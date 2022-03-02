b2e769aec315f21b13f9dfec91e21836a8f1a268
CodingTomato <me@codingtomato.de>
2022-03-02 17:09:55 +0100

    Update 8ball kommand

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,5 +1,5 @@
 module.exports = {
-  alias: ['8ball', 'magischemiesmuschel', 'magische-miesmuschel'],
+  alias: ['8ball', 'magischemiesmuschel', 'magische-miesmuschel', '🐚'],
   async execute(client, channel, args, tags, dbClient) {
     const [message] = args;
 
@@ -10,6 +10,6 @@ module.exports = {
     const answers = ['Ist doch klar.', 'Das ist eindeutig so.', 'Zweifellos.', 'Ja, auf jeden Fall.', 'Sie können sich darauf verlassen.', 'Meiner Meinung nach, ja.', 'Höchstwahrscheinlich.', 'Gute Aussichten.', 'Ja', 'Die Anzeichen deuten auf ein Ja hin.', 'Ist nicht ganz klar, versuchen Sie es erneut.', 'Frag mich später nochmal.', 'Ich sage es dir besser nicht.', 'Kann ich jetzt nicht vorhersagen.', 'Konzentriere dich und frag erneut.', 'Verlass dich nicht darauf.', 'Meine Antwort lautet nein.', 'Meine Quellen sagen nein.', 'Der Ausblick ist nicht so gut.', 'Sehr fraglich.'];
     const answer = answers[Math.round(Math.random() * answers.length)];
 
-    client.say(channel, `Die magische Miesmuschel sagt: ${answer}`);
+    client.say(channel, `🐚: ${answer}`);
   },
 };
