1c4fea2fb8307b6f0cc73a918427c85479783764
CodingTomato <12703962+CodingTomato@users.noreply.github.com>
2022-03-20 12:00:09 +0100

    update emote command

--- commands/emote.js
+++ commands/emote.js
@@ -1,6 +1,6 @@
 module.exports = {
   alias: ['emote', 'emotes'],
   async execute(client, channel) {
-    client.say(channel, 'coding36Bruschetta coding36Close coding36Ketchup Alle Emotes wurden von BlackDesire1305 erstellt. Vielen Dank dafür! TwitchUnity coding36Nice coding36Tomate https://twitch.tv/blackdesire1305');
+    client.say(channel, 'coding36Bruschetta coding36Burning coding36Close Alle Emotes wurden von BlackDesire1305 erstellt. Vielen Dank dafür! TwitchUnity coding36Ketchup coding36Money coding36Nice coding36Tomate https://twitch.tv/blackdesire1305');
   },
 };
